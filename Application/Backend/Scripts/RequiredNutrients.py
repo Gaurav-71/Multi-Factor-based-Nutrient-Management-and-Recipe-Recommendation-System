3f74fe227151210479f8f2f65e2b0f2579483e58
weirdscorpio <dheerajbulldog@gmail.com>
2022-06-20 20:30:05 +0530

    Formatting

--- Application/Backend/Scripts/RequiredNutrients.py
+++ Application/Backend/Scripts/RequiredNutrients.py
@@ -6,7 +6,7 @@ from scipy.spatial.distance import cdist
 
 # %%
 #Returns nutritional requirements for a given user (Mock data for testing)
-def getNutritionalRequirements(MEN, WOMEN,user = pd.Series(["19-24",23.98,2.995,0.38,8.675,24.69,250,1.3,1.5,0.017,0.94,0.185,10,2,50,1.545,120,0.075,12,27,0.105,0.0005,120,289,5.235,1.5]), gender = 'male'):
+def getNutritionalRequirements(MEN, WOMEN, user = pd.Series(["19-24",23.98,2.995,0.38,8.675,24.69,250,1.3,1.5,0.017,0.94,0.185,10,2,50,1.545,120,0.075,12,27,0.105,0.0005,120,289,5.235,1.5]), gender = 'male'):
     def roundOff(n):
         n = round(n, ndigits = 4)
         return n
